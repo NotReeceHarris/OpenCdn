7354474b38e65bceb47f4d500a41902c2c8f5352
Reece Harris <reece.harris98@protonmail.com>
2021-11-22 14:12:56 +0000

    Update credit.js

--- credit.js
+++ credit.js
@@ -1,1 +1,13 @@
-console.log('Developed by Reece Harris (https://github.com/NotReeceHarris)');
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+async function demo() {
+
+
+    await sleep(200000);
+    console.log('Developed by Reece Harris (https://github.com/NotReeceHarris)');
+
+}
+
+demo();
